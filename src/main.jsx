be191de8f276aa1383cbaabe60053737476ed48a
shipatulhasan <shipatulhasan328@gmail.com>
2023-01-16 19:12:13 +0600

    adding live server

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 import './index.css'
 import 'react-loading-skeleton/dist/skeleton.css'
 import AuthProvider from './AuthProvider/AuthProvider'
-axios.defaults.baseURL=('http://localhost:5000')
+axios.defaults.baseURL=('https://video-sharing-app-server.vercel.app')
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
